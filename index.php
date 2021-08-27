d412e2f3df33c059f3914ea063d419a03e8fc339
Bruniele Goulart <brunielegoulartbarbosa@yahoo.com.br>
2021-08-27 14:31:49 -0300

    header e footer

--- index.php
+++ index.php
@@ -4,9 +4,74 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Copo da Felicidade</title>
+    <title>Copo da Felicidade gourmet</title>
+    <style>
+        body{
+            margin: 0;
+            font-family: Verdana, Geneva, Tahoma, sans-serif;
+        }
+
+        header {
+            height:100px;
+            background-color:#FFBCBC;
+            text-align:center;
+            padding: 15px;
+        
+        }
+        .logo {
+            height: 120px;
+        }
+            
+        nav {
+            background-color: #FDEFEF;
+        }
+        nav ul{
+            list-style: none;
+            width: 100%;
+            margin: 0px;
+            padding: 0px;
+            display: flex;
+            justify-content:center ;
+        }
+        nav ul li{
+            padding-top:12px;
+            padding-bottom: 12px;
+            padding-left: 25px;
+            padding-right: 25px;
+        }
+        .content{
+            min-height:400px;
+        }
+        footer{
+            background-color:#FDEFEF;
+            min-height:25px;
+            padding:20px;
+        }
+
+    </style>
 </head>
 <body>
-    
+    <header>
+    <div class="logo"><img src="Imagens/Logo.png" class="logo"></div>
+        
+    </header>
+
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Quem somos</li>
+            <li>Contato</li>
+            <li>Localização</li>
+        </ul>
+    </nav>
+
+    <div class="content">
+        conteudo
+    </div>
+
+    <footer>
+        <p>Bruniele Goulart Barbosa | |  RA: 20038629-5</p>
+        
+    </footer>
 </body>
 </html>
